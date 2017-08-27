e2c38064da9fd99a4787156ed94d1e6c21942386
Finn Huhne <github@finnhuhne.de>
2017-08-27 12:39:19 +0200

    [WIP] Basic UI Setup

--- django/game/views.py
+++ django/game/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def index(request):
-    return HttpResponse("Hello, World. Let me tell you a story...")
+    return render(request, 'game/index.html')
