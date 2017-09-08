52a2dba047dbda349b503926555619825800966b
Finn Huhne <github@finnhuhne.de>
2017-09-08 11:11:26 +0200

    [FEATURE] Add game winning logic
    * Add winning status to template
    * Move tile drop icons above the board
    * Add axis check

--- django/game/models.py
+++ django/game/models.py
@@ -43,8 +43,6 @@ class Board(models.Model):
 
     def drop(self, player: Player, col: int, board: 'Board') -> 'Tile':
         # This method 'drops' a tile into the board
-
-        print(self.current_turn)
         
         if ((self.current_turn != player) and (self.current_turn is not None)) or (self.game_status == 2):
             # This will happen when it's not the players turn
@@ -58,7 +56,10 @@ class Board(models.Model):
                 tile = Tile(board=board, player=player, position_x=col, position_y=row)
                 tile.save()
 
-                self.current_turn = Player.objects.filter(game=self.game).exclude(pk=player.pk).get()
+                self.check_game_status(tile)
+                if self.game_status != 2:
+                    self.current_turn = Player.objects.filter(game=self.game).exclude(pk=player.pk).get()
+
                 self.save()
 
                 return tile
@@ -67,7 +68,106 @@ class Board(models.Model):
         raise Exception('Board seems to be full ...')
 
     def check_game_status(self, tile: 'Tile'):
-        return
+        axis_strings = self.axis_strings(tile.position_x, tile.position_y)
+
+        for axis in axis_strings:
+            last_player = None
+            count = 1
+
+            for index, tile in enumerate(axis_strings[axis]):
+                if tile:
+                    if last_player == tile.player:
+                        count += 1
+                    else:
+                        count = 1
+
+                    if count >= self.win_length:
+                        self.game_status = 2
+                        self.won_by = tile.player
+
+                    last_player = tile.player
+                else:
+                    count = 1
+                    last_player = None
+
+    def axis_strings(self, xc: int, yc: int):
+        """
+        We have the parameters for the coordinate of the tile we just placed.
+        With this, we can check for a game win without checking every field on the board.
+        """
+        xc = int(xc)
+        yc = int(yc)
+
+        # This will preload the index so we don't have to do it before every direction
+        axis_strings = {0: [], 1: [], 2: [], 3: []}
+
+        """
+        Vertical
+        """
+        for index in range(1, self.height + 1):
+            try:
+                tile = self.tile_set.get(position_x=xc, position_y=index)
+                axis_strings[0].append(tile)
+            except ObjectDoesNotExist:
+                axis_strings[0].append(None)
+
+        # print(axis_strings[0])
+
+        """
+        Diagonal 1 -> left top going bottom right
+        """
+        # Get the offset to the starting point. This will be the shorter axis.
+        offset_d1 = xc - 1 if xc - 1 < self.height - yc else self.height - yc
+
+        # Get the max length for this diagonal.
+        xl_d1 = self.width - (xc - offset_d1)  # Count how far we can go to the right (x-axis)
+        yl_d1 = yc + offset_d1  # Count how far we can go down on the y-axis
+        length_d1 = xl_d1 if xl_d1 < yl_d1 else yl_d1  # use the smaller one for the max length
+
+        # go through the diagonal
+        for position_d1 in range(length_d1):
+            # axis_strings[1] += self.board[xc - offset_d1 + position_d1][yc + offset_d1 - position_d1]
+            try:
+                tile = self.tile_set.get(
+                    position_x=(xc - offset_d1 + position_d1),
+                    position_y=(yc + offset_d1 - position_d1)
+                )
+                axis_strings[1].append(tile)
+            except ObjectDoesNotExist:
+                axis_strings[1].append(None)
+
+        """
+        Diagonal 2
+        Same logic as diagonal 1, but going up on the y-axis
+        """
+        offset_d2 = xc - 1 if xc - 1 < yc - 1 else yc - 1
+
+        xl_d2 = self.width - (xc - offset_d2)
+        yl_d2 = self.height - (yc - offset_d2)
+        length_d2 = xl_d2 if xl_d2 < yl_d2 else yl_d2
+
+        for position_d2 in range(length_d2):
+            # axis_strings[2] += self.board[xc - offset_d2 + position_d2][yc - offset_d2 + position_d2]
+            try:
+                tile = self.tile_set.get(
+                    position_x=(xc - offset_d2 + position_d2),
+                    position_y=(yc - offset_d2 + position_d2)
+                )
+                axis_strings[2].append(tile)
+            except ObjectDoesNotExist:
+                axis_strings[2].append(None)
+
+        """
+        Horizontal
+        """
+        for index in range(1, self.width + 1):
+            try:
+                tile = self.tile_set.get(position_x=index, position_y=yc)
+                axis_strings[3].append(tile)
+            except ObjectDoesNotExist:
+                axis_strings[3].append(None)
+
+        return axis_strings
 
 
 # These are the tiles placed on a specified board
